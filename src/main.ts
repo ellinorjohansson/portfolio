9b209ba288060196858f3709797cb97c39968488
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-04-14 16:56:22 +0200

    ✨(feature): add nav menu in mobile

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "./style.scss";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu-button') as HTMLElement;
+    const checkbox = document.getElementById('menuToggle') as HTMLInputElement;
+  
+    menuButton.addEventListener('click', () => {
+      checkbox.checked = !checkbox.checked;
+    });
+  });
+  
