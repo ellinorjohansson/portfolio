c4ff1ff1f813baa060a74f21e2fee627bbb10204
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-07-04 20:29:23 +0200

    ✨(feature): add every component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { AboutMe } from './components/AboutMe'
+import { GetInTouch } from './components/GetInTouch'
+import { Header } from './components/Header'
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <AboutMe/>
+      <Projects/>
+      <Skills/>
+      <GetInTouch/>
     </>
   )
 }
