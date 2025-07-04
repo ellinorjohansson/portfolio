a524e7b3ad0ae82ff71c64593fc67a77c704691a
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-07-04 20:26:56 +0200

    ✨(feature): add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Ellinor Johansson</h1>
+        <nav>
+          <ul>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Get in touch</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
