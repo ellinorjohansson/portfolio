ab6effbf9883b0ecf20987d307e8049e3a0081f7
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-07-08 18:37:01 +0200

    💄(style): basic style to header and menu on tablet/mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './Header.scss'
 
 export const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header>
         <h1>Ellinor Johansson</h1>
-        <nav>
+        <button className={`header-toggle ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)} aria-label='Toggle menu'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className={`header-nav ${menuOpen ? 'open' : ''}`}>
           <ul>
             <li>About me</li>
             <li>Projects</li>
