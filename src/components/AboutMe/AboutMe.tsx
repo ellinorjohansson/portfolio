955b0a31b0a22118e4c3d19abb3a003ff0213a28
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-08 14:12:30 +0200

    💄(style): add basic style to about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,44 +2,46 @@ import './AboutMe.scss'
 
 export const AboutMe = () => {
   return (
-    <>
-      <section>
-        <img src="" alt="" />
-        <h2>Frontend developer</h2>
-        <h3>+ digital marketer</h3>
-      </section>
-      <section>
-        <h2>Hi, glad you found your way here!</h2>
-        <p>
-          I've always been driven by a curiosity for how digital content can
-          capture attention, engage emotions, and influence people. With a
-          degree in Digital Communication, I've learned how to tell stories,
-          build brands, and create meaningful experiences.
-        </p>
-        <p>
-          But the more I explored the digital world, the more I was drawn to the
-          technical side, the part that brings ideas to life. Now, I'm building
-          on that foundation as a Frontend Developer. I love crafting clean,
-          user-friendly web solutions where design meets functionality. I'm
-          currently diving deeper into HTML, CSS, JavaScript, Vue, and React,
-          and I'm always curious about new ways to build interactive and smart
-          solutions.
-        </p>
+    <div className='wrapper'>
+      <div className='info-wrapper'>
+        <section className='short-about-me'>
+          <img src="" alt="" />
+          <h2>Frontend developer</h2>
+          <h3>+ digital marketer</h3>
+        </section>
+        <section>
+          <h2>Hi, glad you found your way here!</h2>
+          <p>
+            I've always been driven by a curiosity for how digital content can
+            capture attention, engage emotions, and influence people. With a
+            degree in Digital Communication, I've learned how to tell stories,
+            build brands, and create meaningful experiences.
+          </p>
+          <p>
+            But the more I explored the digital world, the more I was drawn to the
+            technical side, the part that brings ideas to life. Now, I'm building
+            on that foundation as a Frontend Developer. I love crafting clean,
+            user-friendly web solutions where design meets functionality. I'm
+            currently diving deeper into HTML, CSS, JavaScript, Vue, and React,
+            and I'm always curious about new ways to build interactive and smart
+            solutions.
+          </p>
 
-        <p>
-          To me, frontend development is about blending creativity with
-          technology to create digital experiences that don’t just work but feel
-          right
-        </p>
-      </section>
-      <section>
-        <a href="https://github.com/ellinorjohansson" target="_blank">
-          <i></i>
-        </a>
-        <a href="https://www.linkedin.com/in/ellinor-johansson-957a20224/">
-          <i></i>
-        </a>
-      </section>
-    </>
+          <p>
+            To me, frontend development is about blending creativity with
+            technology to create digital experiences that don’t just work but feel
+            right
+          </p>
+        </section>
+        <section>
+          <a href="https://github.com/ellinorjohansson" target="_blank">
+            <i></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ellinor-johansson-957a20224/">
+            <i></i>
+          </a>
+        </section>
+      </div>
+    </div>
   );
 };
